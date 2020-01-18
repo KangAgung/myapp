0f4cad045cc6c15e3625f5d3da575e2679ba3f21
Agung Tri A <agung.t.atmojo@gmail.com>
2020-01-18 09:07:06 +0700

    edit index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const port = 8000;
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended : true
 }));
 
-app.listen(8000,function (err) {
+app.listen(port,function (err) {
     if (err) throw err;
-    console.log("Server started on port 8000");
+    console.log("Server started on port : "+ port);
 });
