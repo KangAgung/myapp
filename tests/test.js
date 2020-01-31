4762eeba5772514c4a2ae37fcec420a3bf17a360
Agung Tri A <agung.t.atmojo@gmail.com>
2020-01-31 12:53:57 +0700

    add dummy test

--- tests/test.js
+++ tests/test.js
@@ -1,2 +1,12 @@
+const request = require('supertest');
+const app = require('../app');
 
-
+describe('API', function () {
+    describe('GET /', function () {
+        it('should GET all data', function (done) {
+            request(app)
+                .get('/')
+                .expect(200,done);
+        });
+    });
+});
