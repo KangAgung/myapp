93ea31c61e9b75f5cd80e4676ad6cfe846a8a808
Agung Tri A <agung.t.atmojo@gmail.com>
2020-02-01 11:09:56 +0700

    change port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 const app = express();
 
 app.use(bodyParser.json());
